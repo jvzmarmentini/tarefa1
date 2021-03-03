b7e2efa510f7260a5b6f948870b7537840cfea40
jvzmarmentini <jvzmarmentini@gmail.com>
2021-03-03 16:13:12 -0300

    minor changes

--- Analise.java
+++ Analise.java
@@ -1,5 +1,5 @@
 public class Analise {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World");
   }
+}
